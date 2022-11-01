ffdb4c750628845c513c272bd636ac96221a8a07
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2022-11-01 01:46:24 -0400

    new version of code

--- index.js
+++ index.js
@@ -16,162 +16,160 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function validateTeacherData(data) {
-    try {
-        const teacher = {
-            name: `${capitalizeFirstLetter(data.firstName.trim())} ${capitalizeFirstLetter(data.lastName.trim())}`,
-            email: data.email.trim(),
-            password: data.password.trim(),
-            gender: data.gender.trim(),
-            disabled: data.disabled
-        }
-        const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-        const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (!teacher.name || !regName.test(teacher.name)) {
-            return { status: 'error', message: 'Wrong from data provided for name.' }
-        }
-        if (!teacher.email || !regEmail.test(teacher.email)) {
-            return { status: 'error', message: 'Wrong from data provided for email.' }
-        }
-        if (!teacher.password || teacher.password.length < 6) {
-            return { status: 'error', message: 'Wrong from data provided for password.' }
-        }
-        if (!teacher.gender || (teacher.gender !== 'male' && teacher.gender !== 'female')) {
-            return { status: 'error', message: 'Wrong from data provided for gender.' }
-        }
-        if (!teacher.disabled || (teacher.disabled !== 'enabled' && teacher.disabled !== 'disabled')) {
-            return { status: 'error', message: 'Wrong from data provided for enabled/disabled.' }
-        }
-        return createTeacher(teacher)
-
-    }
-    catch (e) {
-        return { status: 'error', message: 'Insufficient data provided.' }
-    }
-
-}
-
-async function createTeacher(teacher) {
-    try {
-        const user = await admin.auth().createUser({
-            email: teacher.email,
-            password: teacher.password,
-            disabled: teacher.disabled === 'disabled' ? true : false
-        })
-        try {
-            await admin.firestore().collection('teachers').doc(user.uid).set({
-                name: teacher.name,
-                email: teacher.email,
-                gender: teacher.gender,
-                id: user.uid,
-                disabled: teacher.disabled === 'disabled' ? true : false
-            })
-            return { status: 'success', message: 'Teacher created successfully.' }
-        }
-        catch (error) {
-            console.log(error.message)
-            return { status: 'error', message: `Teacher couldn't be created successfully in the database.` }
-        }
-    }
-    catch (error) {
-        console.log(error.message)
-        return { status: 'error', message: error.message }
-    }
-}
-
-async function deleteTeacher(data) {
-    try {
-        if (!data.id) {
-            return { status: 'error', message: `Missing teacher ID.` }
-        }
-        await admin.auth().deleteUser(data.id)
-        try {
-            await admin.firestore().collection('teachers').doc(data.id).delete()
-            return { status: 'success', message: `Teacher successfully deleted.` }
-        }
-        catch (error) {
-            return { status: 'error', message: `Teacher couldn't be deleted from the database.` }
-        }
-    }
-    catch (error) {
-        console.log(error.code)
-        return { status: 'error', message: error.message }
-    }
-}
-async function editTeacherName(data) {
-    try {
-        const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-        if (!data.firstName || !data.lastName) {
-            return { status: 'error', message: 'Missing data for name.' }
-        }
-        const fullName = `${capitalizeFirstLetter(data.firstName.trim())} ${capitalizeFirstLetter(data.lastName.trim())}`
-        if (!regName.test(fullName)) {
-            return { status: 'error', message: 'Wrong from data provided for name.' }
-        }
-        await admin.firestore().collection('teachers').doc(data.id).update({ name: fullName })
-        return { status: 'success', message: `Teacher's name successfully updated.` }
-    }
-    catch (error) {
-        console.log(error.code)
-        return { status: 'error', message: error.message }
-    }
-}
-
-async function enableDisableTeacher(data) {
-    try {
-        await admin.auth().updateUser(data.id, { disabled: data.disable })
-        try {
-            await admin.firestore().collection('teachers').doc(data.id).update({ disabled: data.disable })
-            return { status: 'success', message: `Teacher's account successfully ${data.disable ? 'disabled' : 'enabled'}.` }
-        }
-        catch (error) {
-            console.log(error.message)
-            return { status: 'error', message: error.message }
-        }
-    }
-    catch (error) {
-        console.log(error.code)
-        return { status: 'error', message: error.message }
-    }
-}
-
 function checkAuth(req, res, next) {
     if (req.headers.token) {
-        admin.auth().verifyIdToken(req.headers.token)
+        const TOKEN = req.headers.token
+        admin.auth().verifyIdToken(TOKEN)
             .then((user) => {
                 admin.firestore().collection('admins').doc(user.uid).get()
                     .then((userDb) => {
                         if (userDb.data()?.name) {
                             return next()
                         }
-                        res.status(403).send({ error: 'Error in Authenticating request' })
+                        res.status(403).send({ status: "error", message: 'Error in Authenticating request' })
                     })
-                    .catch((e) => {
-                        console.log(e, 'hi')
+                    .catch((error) => {
+                        console.log(error)
                     })
             }).catch(() => {
-                res.status(403).send({ error: 'Error in Authenticating request' })
+                res.status(403).send({ status: "error", message: 'Error in Authenticating request' })
             });
     } else {
-        res.status(403).send({ error: 'No token' })
+        res.status(403).send({ status: "error", message: 'No token' })
     }
 }
+app.use(checkAuth)
 
-app.post('/add_teacher', checkAuth, async (req, res) => {
-    const response = await validateTeacherData(req.body)
-    res.send(response)
+app.post('/add_teacher', async (req, res) => {
+    try {
+        let firstName = []
+        req.body.firstName.trim().split(/\s+/).forEach((name) => {
+            firstName.push(capitalizeFirstLetter(name))
+        });
+        let lastName = []
+        req.body.lastName.trim().split(/\s+/).forEach((name) => {
+            lastName.push(capitalizeFirstLetter(name))
+        });
+        const teacher = {
+            firstName: firstName.join(' '),
+            lastName: lastName.join(' '),
+            email: req.body.email.trim(),
+            password: req.body.password.trim(),
+            gender: req.body.gender.trim(),
+            disabled: req.body.disabled
+        }
+        const regName = /^[a-zA-Z ]+$/;
+        const regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!teacher.firstName || !regName.test(teacher.firstName)) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for first name.' })
+        }
+        if (!teacher.lastName || !regName.test(teacher.lastName)) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for first name.' })
+        }
+        if (!teacher.email || !regEmail.test(teacher.email)) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for email.' })
+        }
+        if (!teacher.password || teacher.password.length < 6) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for password.' })
+        }
+        if (!teacher.gender || (teacher.gender !== 'male' && teacher.gender !== 'female')) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for gender.' })
+        }
+        if (!teacher.disabled || (teacher.disabled !== 'enabled' && teacher.disabled !== 'disabled')) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for enabled/disabled.' })
+        }
+        try {
+            const user = await admin.auth().createUser({
+                email: teacher.email,
+                password: teacher.password,
+                disabled: teacher.disabled === 'disabled' ? true : false
+            })
+            try {
+                await admin.firestore().collection('teachers').doc(user.uid).set({
+                    firstName: teacher.firstName,
+                    lastName: teacher.lastName,
+                    email: teacher.email,
+                    gender: teacher.gender,
+                    id: user.uid,
+                    disabled: teacher.disabled === 'disabled' ? true : false
+                })
+                return res.send({ status: 'success', message: 'Teacher created successfully.' })
+            }
+            catch (error) {
+                console.log(error.message)
+                return res.send({ status: 'error', message: `Teacher couldn't be created successfully in the database.` })
+            }
+        }
+        catch (error) {
+            console.log(error.message)
+            return res.send({ status: 'error', message: error.message })
+        }
+    }
+    catch (error) {
+        return res.send({ status: 'error', message: 'Wrong data types for teacher.' })
+    }
 })
-app.delete('/delete_teacher', checkAuth, async (req, res) => {
-    const response = await deleteTeacher(req.body)
-    res.send(response)
+app.delete('/delete_teacher', async (req, res) => {
+    try {
+        if (!req.body.id) {
+            return res.send({ status: 'error', message: `Missing teacher ID.` })
+        }
+        await admin.auth().deleteUser(req.body.id)
+        try {
+            await admin.firestore().collection('teachers').doc(req.body.id).delete()
+            return res.send({ status: 'success', message: `Teacher successfully deleted.` })
+        }
+        catch (error) {
+            return res.send({ status: 'error', message: `Teacher couldn't be deleted from the database.` })
+        }
+    }
+    catch (error) {
+        console.log(error.code)
+        return res.send({ status: 'error', message: error.message })
+    }
+
 })
-app.put('/edit_teacher_name', checkAuth, async (req, res) => {
-    const response = await editTeacherName(req.body)
-    res.send(response)
+app.put('/edit_teacher_name', cors(), async (req, res) => {
+    try {
+        const regName = /^[a-zA-Z ]+$/;
+        if (!req.body.firstName || !req.body.lastName) {
+            return res.send({ status: 'error', message: 'Missing data for name.' })
+        }
+        if (!regName.test(req.body.firstName) || !regName.test(req.body.lastName)) {
+            return res.send({ status: 'error', message: 'Wrong from data provided for name.' })
+        }
+        let firstName = []
+        req.body.firstName.trim().split(/\s+/).forEach((name) => {
+            firstName.push(capitalizeFirstLetter(name))
+        });
+        let lastName = []
+        req.body.lastName.trim().split(/\s+/).forEach((name) => {
+            lastName.push(capitalizeFirstLetter(name))
+        });
+        await admin.firestore().collection('teachers').doc(req.body.id).update({ firstName: firstName.join(' '), lastName: lastName.join(' ') })
+        return res.send({ status: 'success', message: `Teacher's name successfully updated.` })
+    }
+    catch (error) {
+        console.log(error.code)
+        return res.send({ status: 'error', message: error.message })
+    }
 })
-app.put('/enable_disable_teacher', checkAuth, async (req, res) => {
-    const response = await enableDisableTeacher(req.body)
-    res.send(response)
+app.put('/enable_disable_teacher', async (req, res) => {
+    try {
+        await admin.auth().updateUser(req.body.id, { disabled: req.body.disable })
+        try {
+            await admin.firestore().collection('teachers').doc(req.body.id).update({ disabled: req.body.disable })
+            return res.send({ status: 'success', message: `Teacher's account successfully ${req.body.disable ? 'disabled' : 'enabled'}.` })
+        }
+        catch (error) {
+            console.log(error.message)
+            return res.send({ status: 'error', message: error.message })
+        }
+    }
+    catch (error) {
+        console.log(error.code)
+        return res.send({ status: 'error', message: error.message })
+    }
 })
 
 app.use((req, res) => {
